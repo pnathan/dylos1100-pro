f4f0fee7590fb2a19e4dd02ef78fca2ede026b96
Paul Nathan <paul@nathan.house>
2020-09-16 17:34:31 -0700

    More notes on the system

--- dylos.py
+++ dylos.py
@@ -5,12 +5,16 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s  %(message)s')
 
 
 with serial.Serial('/dev/ttyUSB0', 9600, timeout=31) as ser:
+    # one emit per 60s
+    # this sets up a number that doesn't go into 60 for the timeoutm
     logging.info("reading, timeout 31s")
     line=ser.readline().decode('ascii').strip().split(',')
     if line != '':
         try:
-            d = { '>pm2.5': line[0],
-                  '>pm10' : line[1] }
+            # units are in microns, according to the dylos-1100-pro
+            # manual
+            d = { '>pm0.5': line[0],
+                  '>pm2.5' : line[1] }
             print(json.dumps(d))
         except IndexError:
             print('')
--- dylos.sh
+++ dylos.sh
@@ -5,6 +5,11 @@ while [[ /bin/true ]]; do
     if [[ -s latest.json ]]; then
         echo "data found:"
         cp latest.json previous.json
+
+        # note that this uploads to my own internet data servers. A
+        # more convetional approach would be to plop it into a CSV
+        # denoted by the hour or thereabouts, then use R/Excel to graph it.
+
         cat previous.json | jq ". += {\"dylos\": true, \"hostname\": \"$(hostname)\"} | {\"dict\" : . } | . += {\"name\" : \"dylos\"}" > payload.json
         curl -XPOST -H "X-PMETRICS-API-KEY: <thekey>"  \
              http://upside-down-research.com/pmetrics/api/v1/event \
